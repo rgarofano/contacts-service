1c5e38360cf99a5a36dafc0d4159a3307401dc7f
Ryan Garofano <ryan@ryangarofano.ca>
2025-04-27 17:49:01 -0700

    Refactor config file load to be generic and move to common directory
    Change-Id: I816a9741c4614dbaa1c36f0f068d96a2bf2f74c7

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/rgarofano/contacts-service
 
 go 1.24.2
+
+require github.com/rgarofano/MyCloud v0.0.0
+// just for local development purposes
+replace github.com/rgarofano/MyCloud => ../
--- main.go
+++ main.go
@@ -1,45 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
+	"github.com/rgarofano/MyCloud/common"
 )
 
-type config struct {
+type contactsServiceConfig struct {
 	Port int `json:"port"`
 }
 
-func loadConfig() (*config, error) {
-	jsonFile, err := os.Open("config.json")
-	if err != nil {
-		return nil, err
-	}
-	defer jsonFile.Close()
-
-	jsonBytes, err := io.ReadAll(jsonFile)
-	if err != nil {
-		return nil, err
-	}
-
-	var cfg config
-	err = json.Unmarshal(jsonBytes, &cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	return &cfg, nil
-}
-
 func main() {
-	config, err := loadConfig()
+	var cfg contactsServiceConfig
+	err := config.Load("./config.json", &cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Starting contacts service on port %d", config.Port)
-	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
+	log.Printf("Starting contacts service on port %d", cfg.Port)
+	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil)
 }
