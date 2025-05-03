9b4ccc94af75db92ab10e32f00234edf7c2c49dc
Ryan Garofano <ryan@ryangarofano.ca>
2025-05-03 11:19:04 -0700

    Bump tag for parent repo and refactor to proper init function
    Change-Id: Ibf3b275bcac5cb5c593497d9221e671239bc6a53

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/rgarofano/contacts-service
 
 go 1.24.2
 
-require github.com/rgarofano/MyCloud v0.1.0 // indirect
+require github.com/rgarofano/MyCloud v0.1.1
--- main.go
+++ main.go
@@ -2,28 +2,45 @@ package main
 
 import (
 	"fmt"
+	"github.com/rgarofano/MyCloud/common"
 	"log"
 	"net/http"
-	"github.com/rgarofano/MyCloud/common"
+	"os"
 )
 
 type contactsServiceConfig struct {
-	Port int `json:"port"`
+	Port             int    `json:"port"`
+	ContactsFileName string `json:"contactsFileName"`
 }
 
 type contact struct {
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
 	PhoneNumber string `json:"phoneNumber"`
 }
 
-func main() {
-	var cfg contactsServiceConfig
-	err := config.Load("./config.json", &cfg)
+var cfg contactsServiceConfig
+var contacts []contact
+
+func init() {
+	err := utils.LoadJson("config.json", &cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	if _, err := os.Stat(cfg.ContactsFileName); err == nil {
+		err = utils.LoadJson(cfg.ContactsFileName, &contacts)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if os.IsNotExist(err) {
+		contacts = make([]contact, 0)
+	} else {
+		log.Fatal(err)
+	}
+}
+
+func main() {
 	log.Printf("Starting contacts service on port %d", cfg.Port)
 	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil)
 }
